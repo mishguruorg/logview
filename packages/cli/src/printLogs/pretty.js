b1299903d22d8284af145e09a62cbb2d25905170
George Czabania <george@mish.guru>
2019-02-13 21:11:39 +1300

    Handle different length ids

--- packages/cli/src/printLogs/pretty.js
+++ packages/cli/src/printLogs/pretty.js
@@ -6,7 +6,6 @@ import leftPad from 'left-pad'
 
 const column = (text, size, pad = rightPad) => pad(text, size, ' ').slice(0, size).replace(/\n/g, '')
 
-const ID_SIZE = 10
 const SENT_FROM_SIZE = 22
 const TYPE_SIZE = 40
 const DATE_SIZE = 25
@@ -26,15 +25,20 @@ const formatPayload = (payload, size) => {
 }
 
 const printLogs = (results) => {
+  if (results.length === 0) {
+    return
+  }
+
   const { width } = getWindowSize()
 
-  const infoSize = width - ID_SIZE - SENT_FROM_SIZE - TYPE_SIZE - DATE_SIZE
+  const idSize = results[0].id.toString().length + 1
+  const infoSize = width - idSize - SENT_FROM_SIZE - TYPE_SIZE - DATE_SIZE
 
   for (const log of results) {
     const { id, type, payload, sentFrom, sentAt } = log
     const formattedSentAt = DateTime.fromISO(sentAt).toFormat('FF')
     console.log([
-      chalk.blue(column(id, ID_SIZE)),
+      chalk.blue(column(id, idSize)),
       chalk.white(column(sentFrom || '[unknown]', SENT_FROM_SIZE)),
       chalk.green(column(type, TYPE_SIZE)),
       formatPayload(payload, infoSize),
